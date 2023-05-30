f337601971b29501dc6c10032e8395cab9a84ca4
TungDcode <75203313+TungDcode@users.noreply.github.com>
2023-05-30 15:53:06 +0700

    Update VuetableCssTailwindUI.js

--- src/components/VuetableCssTailwindUI.js
+++ src/components/VuetableCssTailwindUI.js
@@ -3,7 +3,7 @@ export default {
     tableWrapper: "md:rounded-lg",
     thead: "bg-tertiary-100 font-medium",
     tbody: "divide-y divide-tertiary-200 bg-white font-normal",
-    table: "min-w-full rounded-md overflow-hidden",
+    table: "min-w-full rounded-md",
     th: "hidden px-3 py-smSpace text-left text-sm text-sm font-medium text-tertiary-700 lg:table-cell",
     ascendingIcon: '<svg fill="#2185d0" viewBox="0 0 24 24" width="14" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">\n' +
       '  <path clip-rule="evenodd" fill-rule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z"></path>\n' +
